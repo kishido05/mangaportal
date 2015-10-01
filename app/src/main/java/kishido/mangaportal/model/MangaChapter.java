b24254db7b82a6c1a2051443d0b3630dbd015e00
Paulo Carabuena <carabuena.paulo@gmail.com>
2015-10-01 20:40:35 +0800

    Create manga chapter model

---
+++ app/src/main/java/kishido/mangaportal/model/MangaChapter.java
@@ -0,0 +1,35 @@
+package kishido.mangaportal.model;
+
+/**
+ * Created by syspaulo on 10/1/2015.
+ */
+public class MangaChapter {
+
+    protected String name;
+    protected String number;
+    protected String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
