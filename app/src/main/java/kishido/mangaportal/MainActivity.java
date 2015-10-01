b82e3c6bc03a0e94a25c639a56ae8b74267f5b0e
Paulo Carabuena <carabuena.paulo@gmail.com>
2015-10-01 18:12:48 +0800

    Test crawling for Mangafox

--- app/src/main/java/kishido/mangaportal/MainActivity.java
+++ app/src/main/java/kishido/mangaportal/MainActivity.java
@@ -2,15 +2,28 @@ package kishido.mangaportal;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import kishido.mangaportal.adapter.MangaListAdapter;
+import kishido.mangaportal.crawler.Crawler;
+import kishido.mangaportal.crawler.impl.MangafoxCrawlAlgorithm;
 
 /**
  * Created by syspaulo on 9/27/2015.
  */
 public class MainActivity extends AppCompatActivity {
 
+    private MangaListAdapter arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_main);
+
+        ListView list = (ListView) findViewById(R.id.listManga);
+        arrayAdapter = new MangaListAdapter(this);
+        list.setAdapter(arrayAdapter);
+
+        Crawler.lookup(Crawler.Site.MANGA_FOX, new MangafoxCrawlAlgorithm(arrayAdapter));
     }
 }
